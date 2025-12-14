007a5d8921e6154003627ed63aecb39e294b549e
tayeeb-office <tayeeb.office@gmail.com>
2025-12-14 19:00:38 +0600

    commit 02

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://assignment_11:8zYDGilWnFnjPBl3@cluster0.0nyvlxc.mongodb.net/?appName=Cluster0";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://assignment_11:8zYDGilWnFnjPBl3@cluster0.0nyvlxc.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,18 +25,37 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    
-    
-    
+
+    const database = client.db("assignment_11");
+    const userCollections = database.collection("user");
+
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.role = "donor";
+      userInfo.status = "active";
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result);
+    });
+
+    app.get("/users/role/:email", async (req, res) => {
+      const { email } = req.params;
+
+      const query = { email };
+      const result = await userCollections.findOne(query);
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Hello Developers");
@@ -45,4 +64,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`server is runnion on ${port}`);
 });
- 
